171e30f488f3930d808bb9a529596f641b2569ec
actor20170211030627 <1455198886@qq.com>
2026-08-10 18:33:15 +0800

    update to 1.0.0.6, Rrrrrredid publish.yml

--- CustomMessageBoxDemo/Properties/AssemblyInfo.cs
+++ CustomMessageBoxDemo/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.5")]
-[assembly: AssemblyFileVersion("1.0.0.5")]
-[assembly: AssemblyInformationalVersion("1.0.0.5(202608101815)")]
+[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("1.0.0.6")]
+[assembly: AssemblyInformationalVersion("1.0.0.6(202608101832)")]
